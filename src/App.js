78b63519b10a5c51f9a4b07a6c209b6294f6f178
shashankkatte <shashankanand.katte@gmail.com>
2021-02-06 23:23:49 -0500

    Src : Add Table component
    Add and Bind data to a sorted table component by country with highest case count. Add styling for overflow scroll.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import {
 } from '@material-ui/core';
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
 import './App.css';
+import { sortData } from './util';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -35,6 +38,8 @@ function App() {
             name: country.country, //United kingdom, France
             value: country.countryInfo.iso2, //UK, US, FR
           }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -60,8 +65,8 @@ function App() {
       });
   };
 
-  console.log('country info >>>>', countryInfo);
-  console.log('country code >>>>>', country);
+  // console.log('country info >>>>', countryInfo);
+  // console.log('country code >>>>>', country);
 
   return (
     <div className="app">
@@ -107,7 +112,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live cases by country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
           {/* Graph */}
         </CardContent>
