4e4957259e3d8ad0a284cec8ff0a08d7ba4473fc
shashankkatte <shashankanand.katte@gmail.com>
2021-02-06 19:04:12 -0500

    src : get basic dropdown with Material UI
    Install Material UI and show up basic country selector with hard coded values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import React from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from '@material-ui/core';
 import './App.css';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build covid tracker!</h1>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant = "outlined"
+          value ="abc"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="USA">USA</MenuItem>
+          <MenuItem value="Canada">Canada</MenuItem>
+          <MenuItem value="India">India</MenuItem>
+        </Select>
+      </FormControl>
+      {/*Header*/}
+      {/* Title + Select input dropdown */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+       {/* Table */}
+       {/* Graph */}
+
+       {/* Map */}
+
     </div>
   );
 }
